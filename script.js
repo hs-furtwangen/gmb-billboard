489c0d48fca34bb175003a99eeffd4f3205b3bd0
Norbert Schnell <norbert@schnell.space>
2026-03-17 01:23:20 +0100

    included slide count

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
-const frameElem = document.getElementById('iframe');
+const frameElem = document.getElementById('google-slides-iframe');
+const params = new URLSearchParams(document.location.search);
+const slideDuration = 7000;
 
-setInterval(reloadFrame, 20000);
+const presentationId = `1FGLyRgIRlXaOdxa80KfoXJEbW2uRzYwAMNYo6CBS-IQ`;
+const apiKey = 'AIzaSyCaP7wotYiSvym_sQO0VDSfq1GgVmrMUQw';
+const slidesURL = 'https://docs.google.com/presentation/d/e/2PACX-1vS_LeQpzmuW-7ht24cwq_ih3EjSB-RTy1_Q6rMig950jbq9YPfGyXvdzApaG2hhx_Rj5rdGTFB_aexH';
+const pdfUrl = `https://docs.google.com/presentation/d/${presentationId}/export/pdf`;
 
-function reloadFrame() {
-  frameElem.src += '';
+(async function main() {
+  const autoPlay = (params.get("auto-play") === 'true');
+
+  if (autoPlay) {
+    loadFrameAuto();
+  } else {
+    frameElem.src = `${slidesURL}/embed`;
+  }
+})();
+
+async function getNumSlides() {
+  const result = await fetch(pdfUrl);
+  const arrayBuffer = await result.arrayBuffer();
+  const pdfDoc = await PDFLib.PDFDocument.load(arrayBuffer);
+  return pdfDoc.getPageCount();
+}
+
+async function loadFrameAuto() {
+  const numSlides = await getNumSlides();
+  frameElem.src = `${slidesURL}/embed?rm=minimal&start=true&delayms=${slideDuration}&loop=false`;
+  setTimeout(loadFrameAuto, numSlides * slideDuration);
 }
